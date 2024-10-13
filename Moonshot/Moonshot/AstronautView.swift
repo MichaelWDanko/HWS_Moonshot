95f381f0f1973ed36c3ff3414853ed6bd72db232
MichaelWDanko <michaeldanko@icloud.com>
2024-10-13 00:23:41 -0400

    Adds new AstronautView and links all views together with selected context

---
+++ Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Michael Danko on 10/13/24.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
